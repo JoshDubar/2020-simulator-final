8a0fc28b702123c9c86d5c110ed19bf4fcafeacb
Andy Lin <andylinnz79@gmail.com>
2020-08-23 16:38:00 +1000

    everything except friendship bars

--- 2020-simulator/Assets/Scripts/CreateInnerRadius.cs
+++ 2020-simulator/Assets/Scripts/CreateInnerRadius.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateRadius : MonoBehaviour
+public class CreateInnerRadius : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject circle;
@@ -12,12 +12,6 @@ public class CreateRadius : MonoBehaviour
         GameObject range = Instantiate(circle) as GameObject;
         range.transform.parent = this.transform;
         range.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - offset);
-        
-        if (this.transform.tag == "Rando") {
-            //range.AddComponent<DrawCircle>();
-        } 
-        // Green
-        // Red range.GetComponent<SpriteRenderer>().colour = new Color(195, 83, 85, 50);
     }
 
 }
