81ebad917d52a9cab3b336bfb25b12b73253e6ec
Stefan Armando Marbun <StefanMarbun@192-168-1-107.tpgi.com.au>
2020-08-23 22:37:56 +1000

    tweaked karens

--- 2020-simulator/Assets/Scripts/InnerRadiusInteractions.cs
+++ 2020-simulator/Assets/Scripts/InnerRadiusInteractions.cs
@@ -9,7 +9,7 @@ public class InnerRadiusInteractions : MonoBehaviour
             if (collider.transform.parent.parent.tag == "Karen") {
                 Debug.Log("Karen is now going to chase you");
                 collider.transform.GetComponentInParent<KarenController>().following = true;
-                collider.GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.red);
+                collider.transform.parent.GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.red);
             }
         }
     }
